4d2b2067004e25c2e543eda91a75bfadc6e40453
pavankumarch <pavankumarch@ICS-LAP-2324>
2023-01-09 18:32:04 +0530

    Add project files.

---
+++ WeekTest7/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WeekTest7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtEmployeeNo = new System.Windows.Forms.TextBox();
+            this.TxtEmployeeName = new System.Windows.Forms.TextBox();
+            this.TxtSalary = new System.Windows.Forms.TextBox();
+            this.BtnAddnew = new System.Windows.Forms.Button();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TxtEmployeeNo
+            // 
+            this.TxtEmployeeNo.Location = new System.Drawing.Point(317, 75);
+            this.TxtEmployeeNo.Name = "TxtEmployeeNo";
+            this.TxtEmployeeNo.Size = new System.Drawing.Size(100, 20);
+            this.TxtEmployeeNo.TabIndex = 0;
+            // 
+            // TxtEmployeeName
+            // 
+            this.TxtEmployeeName.Location = new System.Drawing.Point(317, 130);
+            this.TxtEmployeeName.Name = "TxtEmployeeName";
+            this.TxtEmployeeName.Size = new System.Drawing.Size(100, 20);
+            this.TxtEmployeeName.TabIndex = 1;
+            // 
+            // TxtSalary
+            // 
+            this.TxtSalary.Location = new System.Drawing.Point(317, 199);
+            this.TxtSalary.Name = "TxtSalary";
+            this.TxtSalary.Size = new System.Drawing.Size(100, 20);
+            this.TxtSalary.TabIndex = 2;
+            // 
+            // BtnAddnew
+            // 
+            this.BtnAddnew.Location = new System.Drawing.Point(174, 274);
+            this.BtnAddnew.Name = "BtnAddnew";
+            this.BtnAddnew.Size = new System.Drawing.Size(75, 23);
+            this.BtnAddnew.TabIndex = 3;
+            this.BtnAddnew.Text = "Add New";
+            this.BtnAddnew.UseVisualStyleBackColor = true;
+            this.BtnAddnew.Click += new System.EventHandler(this.BtnAddnew_Click);
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.Location = new System.Drawing.Point(317, 274);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(75, 23);
+            this.BtnReset.TabIndex = 4;
+            this.BtnReset.Text = "Reset";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Employee No";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(171, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Employee Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(171, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Salary";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(495, 89);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Employee Type";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(51, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(56, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Payroll";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(51, 58);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Consultant";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.BtnAddnew);
+            this.Controls.Add(this.TxtSalary);
+            this.Controls.Add(this.TxtEmployeeName);
+            this.Controls.Add(this.TxtEmployeeNo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtEmployeeNo;
+        private System.Windows.Forms.TextBox TxtEmployeeName;
+        private System.Windows.Forms.TextBox TxtSalary;
+        private System.Windows.Forms.Button BtnAddnew;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ WeekTest7/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WeekTest7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddnew_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["HRCon"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("AddEmployeeByValidating", con);
+            cmd.CommandType= CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@EmployeeNo",TxtEmployeeNo.Text);
+            cmd.Parameters.AddWithValue("@EmployeeName", TxtEmployeeName.Text);
+            cmd.Parameters.AddWithValue("@Salary", TxtSalary.Text);
+            if(radioButton1.Checked)
+            {
+                cmd.Parameters.AddWithValue("@EmployeeType",'P');
+            }
+            cmd.Parameters.AddWithValue("@EmployeeType", 'C');
+            
+            cmd.Parameters.AddWithValue("@EmployeeType", TxtSalary.Text);
+            if(con.State==ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            
+            
+            
+            cmd.ExecuteNonQuery();
+        }
+        public void ResetDetails()
+        {
+            TxtEmployeeNo.Text = "";
+            TxtEmployeeName.Text = "";
+            TxtEmployeeName.Text = "";
+            TxtSalary.Text = "";
+        }
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            
+            ResetDetails();
+
+
+        }
+        
+    }
+}
